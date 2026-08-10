ee8db84622d7da8a3174c3b5d51a83c02ee710a7
Daniel-V <danielvaca@uees.edu.ec>
2026-08-10 09:14:23 -0500

    refactor: ObjetoQueCae solo cae, la destruccion la maneja el GarbageController

--- Assets/Scripts/Catcher/ObjetoQueCae.cs
+++ Assets/Scripts/Catcher/ObjetoQueCae.cs
@@ -4,26 +4,9 @@ public class ObjetoQueCae : MonoBehaviour
 {
     public float velocidadCaida = 3f;
 
-    private Camera camara;
-    private float limiteInferior;
-
-    void Start()
-    {
-        camara = Camera.main;
-        // El borde de abajo de la pantalla, con 1 unidad extra para que se
-        // destruya un poquito despues de desaparecer de la vista.
-        limiteInferior = camara.transform.position.y - camara.orthographicSize - 1f;
-    }
-
     void Update()
     {
-        // Caer hacia abajo
+        // Solo caer. La destruccion la maneja el GarbageController del fondo.
         transform.Translate(Vector3.down * velocidadCaida * Time.deltaTime);
-
-        // Si ya salio por abajo, destruirlo (asi no se acumulan infinitos)
-        if (transform.position.y < limiteInferior)
-        {
-            Destroy(gameObject);
-        }
     }
 }
